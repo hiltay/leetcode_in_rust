146a38cf1d23d41a9661763744aa8ab67492be92
hiltay <904108079@qq.com>
2023-04-26 22:40:10 +0800

    1031

--- src/bin/1031.rs
+++ src/bin/1031.rs
@@ -1,4 +1,7 @@
+use std::cmp;
+
 struct Solution;
+/* s1: O(n^2) 暴力求解
 impl Solution {
     pub fn max_sum_two_no_overlap(nums: Vec<i32>, first_len: i32, second_len: i32) -> i32 {
         let res = Solution::bc(
@@ -13,7 +16,7 @@ impl Solution {
         }else{
             0
         }
-        
+
     }
 
     fn bc(arr: &Vec<i32>, first_len: usize, second_len: usize, start: usize, end: usize)->Vec<i32> {
@@ -52,8 +55,41 @@ impl Solution {
         (sums,range)
     }
 }
+*/
+// s2：O(n) 前缀和 see:https://leetcode.cn/problems/maximum-sum-of-two-non-overlapping-subarrays/solution/tu-jie-mei-you-si-lu-yi-zhang-tu-miao-do-3lli/
+
+impl Solution {
+    pub fn max_sum_two_no_overlap(nums: Vec<i32>, first_len: i32, second_len: i32) -> i32 {
+        let first_len = first_len as usize;
+        let second_len = second_len as usize;
+        let prefix_sum = Solution::prefix_sum(&nums);
+        let mut result = 0;
+        let mut lsum = 0;
+        let mut rsum = 0;
+        for i in first_len+second_len..=nums.len(){
+            println!("{}",i);
+            lsum = cmp::max(lsum, prefix_sum[i-second_len] - prefix_sum[i-first_len-second_len]);
+            rsum = cmp::max(rsum, prefix_sum[i-first_len] - prefix_sum[i-first_len-second_len]);
+            let max = cmp::max(lsum+prefix_sum[i]-prefix_sum[i-second_len], rsum+prefix_sum[i]-prefix_sum[i-first_len]);
+            result = cmp::max(max, result);
+        }
+        result
+    }
+
+    fn prefix_sum(arr: &Vec<i32>) -> Vec<i32> {
+        let mut res = vec![0];
+        if arr.len() != 0 {
+            let mut x = 0;
+            for i in arr {
+                x += i;
+                res.push(x);
+            }
+        }
+        return res;
+    }
+}
 fn main() {
-    let nums = vec![2,1,5,6,0,9,5,0,3,8];
+    let nums = vec![2, 1, 5, 6, 0, 9, 5, 0, 3, 8];
     let result = Solution::max_sum_two_no_overlap(nums, 4, 3);
     println!("{}", result);
 }
