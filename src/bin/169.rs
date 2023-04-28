29b2428002c2c309c968185fce332a8674e21c33
yyyz <904108079@qq.com>
2023-04-28 17:57:50 +0800

    169

---
+++ src/bin/169.rs
@@ -0,0 +1,9 @@
+struct Solution;
+impl Solution {
+    pub fn majority_element(nums: Vec<i32>) -> i32 {}
+}
+fn main() {
+    let nums = vec![2, 2, 1, 1, 1, 2, 2];
+    let result = Solution::majority_element(nums);
+    println!("{}", result);
+}
