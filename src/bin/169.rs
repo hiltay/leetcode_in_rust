08dbfd14c02c6d34984f125f36411247c76e6d75
yyyz <904108079@qq.com>
2023-05-04 14:29:46 +0800

    169

--- src/bin/169.rs
+++ src/bin/169.rs
@@ -3,14 +3,20 @@ impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
         let mut cur_bit_count = 0;
         let mut result = 0;
-        for cur_bit in 0..31 {
-            let mask = 2_i32.pow(cur_bit);
+        let mut mask = 0;
+        let half_length = nums.len()/2;
+        for _ in 0..31 {
+            mask = if mask != 0 {
+                mask << 1
+            }else{
+                1
+            };
             for n in nums.iter() {
                 if n & mask != 0 {
                     cur_bit_count += 1;
                 }
             }
-            if cur_bit_count > nums.len() / 2 {
+            if cur_bit_count > half_length {
                 result += mask;
             }
             cur_bit_count = 0;
@@ -19,7 +25,7 @@ impl Solution {
     }
 }
 fn main() {
-    let nums = vec![2, 2, 1, 1, 1, 2, 2,1,1,1,1];
+    let nums = vec![3,2,3];
     let result = Solution::majority_element(nums);
     println!("{}", result);
 }
