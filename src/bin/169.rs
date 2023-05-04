6adb50b28c14f2116d10de8c94e37359b372e9f6
yyyz <904108079@qq.com>
2023-05-04 15:20:56 +0800

    169

--- src/bin/169.rs
+++ src/bin/169.rs
@@ -1,27 +1,46 @@
 struct Solution;
+// impl Solution {
+//     pub fn majority_element(nums: Vec<i32>) -> i32 {
+//         let mut cur_bit_count = 0;
+//         let mut result = 0;
+//         let mut mask = 0;
+//         let half_length = nums.len() / 2;
+//         for _ in 0..31 {
+//             mask = if mask != 0 { mask << 1 } else { 1 };
+//             for n in nums.iter() {
+//                 if n & mask != 0 {
+//                     cur_bit_count += 1;
+//                 }
+//             }
+//             if cur_bit_count > half_length {
+//                 result += mask;
+//             }
+//             cur_bit_count = 0;
+//         }
+//         result
+//     }
+// }
+
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
-        let mut cur_bit_count = 0;
-        let mut result = 0;
-        let mut mask = 0;
-        let half_length = nums.len() / 2;
-        for _ in 0..31 {
-            mask = if mask != 0 { mask << 1 } else { 1 };
-            for n in nums.iter() {
-                if n & mask != 0 {
-                    cur_bit_count += 1;
-                }
+        let mut majority_candidate = 0;
+        let mut count = 0;
+        for item in nums {
+            if count == 0 {
+                majority_candidate = item;
             }
-            if cur_bit_count > half_length {
-                result += mask;
+
+            if item == majority_candidate {
+                count += 1;
+            } else {
+                count -= 1;
             }
-            cur_bit_count = 0;
         }
-        result
+        majority_candidate
     }
 }
 fn main() {
-    let nums = vec![3, 2, 3];
+    let nums = vec![3, 3, 4];
     let result = Solution::majority_element(nums);
     println!("{}", result);
 }
