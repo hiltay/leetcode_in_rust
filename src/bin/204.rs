bf17711d4e08094202b5325a4f68fc90b0f85516
yyyz <904108079@qq.com>
2023-05-18 18:48:08 +0800

    204

---
+++ src/bin/204.rs
@@ -0,0 +1,33 @@
+struct Solution;
+impl Solution {
+    pub fn count_primes(n: i32) -> i32 {
+        let mut count = 0;
+        for i in 2..n {
+            if i == 2 {
+                count += 1;
+            } else if i % 2 != 0 {
+                // odd
+                let mut is_prime = true;
+                let mut cur = 3;
+                while cur < i {
+                    if i % cur == 0 {
+                        // not prime
+                        is_prime = false;
+                        break;
+                    }
+                    cur +=1;
+                }
+                if is_prime {
+                    count += 1;
+                }
+
+            }
+        }
+        count
+    }
+}
+
+fn main() {
+    let result = Solution::count_primes(499979);
+    println!("{}", result);
+}
