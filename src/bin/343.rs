ab0f3f7b34746dea2c71dd30bfdae7d26397fc27
hiltay <904108079@qq.com>
2023-08-08 22:24:14 +0800

    120

---
+++ src/bin/343.rs
@@ -0,0 +1,16 @@
+use std::collections::HashSet;
+struct Solution;
+impl Solution {
+    fn recursive(n:i32) ->i32{
+
+    }
+
+
+    pub fn integer_break(n: i32) -> i32 {
+
+    }
+}
+fn main() {
+    let result = Solution::integer_break(4);
+    println!("{}", result);
+}
