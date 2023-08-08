d38d6f24e42dd7e8da8dbc2b89fad4d0a901c087
hiltay <904108079@qq.com>
2023-08-08 23:05:30 +0800

    343

--- src/bin/343.rs
+++ src/bin/343.rs
@@ -1,16 +1,35 @@
-use std::collections::HashSet;
 struct Solution;
 impl Solution {
-    fn recursive(n:i32) ->i32{
-
+    fn recursive(n: i32, mut mem: &mut Vec<i32>) -> i32 {
+        if n <= 2 {
+            return 1;
+        }
+        if mem[(n-1) as usize] != 0{
+            return mem[(n-1) as usize];
+        }
+        let mut max = 0;
+        for i in 1..n {
+            let mut res = Solution::recursive(n - i,&mut mem);
+            let mut t = n - i;
+            if res > n - i {
+                t = res;
+            } else {
+                t = n - i
+            }
+            if i * t > max {
+                max = i * t;
+            }
+        }
+        mem[(n-1) as usize] = max;
+        return max;
     }
 
-
     pub fn integer_break(n: i32) -> i32 {
-
+        let mut mem = vec![0; n as usize];
+        return Solution::recursive(n, &mut mem);
     }
 }
 fn main() {
-    let result = Solution::integer_break(4);
+    let result = Solution::integer_break(58);
     println!("{}", result);
 }
